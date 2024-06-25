eeb512277b3305ee75b13351273b610815eaa29d
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-06-25 23:49:48 +0530

    Create 07) One_line_print_vs_multi_line_print.py

---
+++ 07) One_line_print_vs_multi_line_print.py
@@ -0,0 +1,12 @@
+# If you want to print everything in one line , just do .
+
+x,y=int(input("Enter the values of x and y").split(","))
+# note while doing the one that is given in the above line , make sure that you give commas in the input.
+
+print(x,y)
+
+# If you want to print the variables in multiple lines , just do :
+
+print(x)
+print(y)
+
