9261ea3f52f15235147dd75b5b1e7a83907a833d
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-06-25 23:42:53 +0530

    Create Taking_multiple_python_inputs.py

---
+++ Taking_multiple_python_inputs.py
@@ -0,0 +1,9 @@
+x,y=input("Enter two numbers separated by commas").split(",")
+
+print(x)
+print(y)
+
+# Note that the print statement in python consists of /n after it by default.
+# If you want to print everything in one line , just do .
+
+print(x,y)
