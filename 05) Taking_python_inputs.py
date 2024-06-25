877f8946e5cf13723e0be03fc8229edc6b9c0f10
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-06-25 23:36:54 +0530

    Update 05) Taking_python_inputs.py

--- 05) Taking_python_inputs.py
+++ 05) Taking_python_inputs.py
@@ -1,4 +1,6 @@
 # In python , the input values are taken as strings
+# we need to typecast them into others so as to get in our wanted format
+# typecasting is an important element in python.
 
 n=int(input("enter the number"))
 
