6a5022870fd102252c10c0a035aff65c1fe77a23
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-06-26 15:02:27 +0530

    Update merging_args_and_kwargs.py

--- merging_args_and_kwargs.py
+++ merging_args_and_kwargs.py
@@ -1,3 +1,8 @@
+# we can have args and kwargs in the same function as well.
+# we just need to ensure that the kwargs/args in the function definition should have the same order as kwargs and args in the 
+# function calling .Also note that the kwargs/keyword arguments in the function call/defintion should always be 
+# written after the args/positional argument.
+
 def emp_info(*ratings,**data):
     print(type(data))
     print(data)
