9ce7ce763aa2ddf49af5a1db97a9f7e435fd2afe
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-06-26 12:25:23 +0530

    Update 19) default_parameters.py

--- 19) default_parameters.py
+++ 19) default_parameters.py
@@ -1,3 +1,7 @@
+# What are default parameters ? 
+# default parameters are the parameters that have default values on them , when no value is assigned to them.
+
+
 # all the times, the default parameters should be at the end 
 # if you do not keep them at end , then you will receive an error.
 def summ(num1,num2=30):
