c198f88692980b0b2da852d0e917e06b2bfb61e6
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-06-26 10:24:48 +0530

    Create 12) if-elif-then.py

---
+++ 12) if-elif-then.py
@@ -0,0 +1,10 @@
+# note that when we use if-elif-else loop, if a condition is caught then that is the end of that
+
+x=int(input("enter the number"))
+if x>0:
+    print("positive")
+elif(x==0):
+    print("zero")
+else:
+    print("negative")
+    
