8fc3cdb45835a945c3d4b864af66bdc3177141b2
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-06-26 10:59:02 +0530

    Create 17) Amazing_way_of_passing_parameters_in_functions.py

---
+++ 17) Amazing_way_of_passing_parameters_in_functions.py
@@ -0,0 +1,9 @@
+# AMAZING WAY FOR TAKING INPUTS IN FUNCTION:
+# 1. Write a program to create a function that takes two arguments, name and age, and print their value
+
+def func(name,age):
+    print(name)
+    print(age)
+
+
+func(input("enter the name"),int(input("enter the age")))
