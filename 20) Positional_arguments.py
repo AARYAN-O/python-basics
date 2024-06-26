c46b777c9bb86405b70fa4b9c6d585ef9fd428b6
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-06-26 12:26:38 +0530

    Create 20) Positional_arguments.py

---
+++ 20) Positional_arguments.py
@@ -0,0 +1,8 @@
+# in case of positional arguments, its mandatory to follow the same names
+
+def pos(ename,empno,city):
+    print(ename)
+    print(empno)
+    print(city)
+    
+pos('Amit',1001,'hyd')
