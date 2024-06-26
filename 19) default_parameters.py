2066106ec534b498cf6e178ab7a19f989bf30cad
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-06-26 12:24:06 +0530

    Update 19) default_parameters.py

--- 19) default_parameters.py
+++ 19) default_parameters.py
@@ -1,5 +1,5 @@
 # all the times, the default parameters should be at the end 
-
+# if you do not keep them at end , then you will receive an error.
 def summ(num1,num2=30):
     print(num1+num2)
     
