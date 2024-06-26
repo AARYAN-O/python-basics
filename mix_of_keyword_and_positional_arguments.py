62155ec7862821e5f56275c5faaeaee5c7b669db
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-06-26 12:31:59 +0530

    Create mix_of_keyword_and_positional_arguments.py

---
+++ mix_of_keyword_and_positional_arguments.py
@@ -0,0 +1,13 @@
+# yes we can have it 
+def pos(hmm,city,empno,ename):
+    print(hmm)
+    print(ename)
+    print(empno)
+    print(city)
+    
+pos(23,ename='Amit',empno=1001,city='hyd')
+# keyword parameters: function call
+# default parameters: function definition
+# in the line just above all the keyword arguments must be at the end.
+# Conclusion:in both function call and function definition , we need to have all the positional parameters should 
+# be at the end.
