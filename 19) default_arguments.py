810bd878934782e617868077ad9ad3c4e7c86034
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-06-26 12:29:51 +0530

    Update and rename 19) default_parameters.py to 19) default_arguments.py

---
+++ 19) default_arguments.py
@@ -0,0 +1,10 @@
+# What are default arguments ? 
+# default arguments are the arguments that have default values on them , when no value is assigned to them.
+
+
+# all the times, the default arguments should be at the end 
+# if you do not keep them at end , then you will receive an error.
+def summ(num1,num2=30):
+    print(num1+num2)
+    
+summ(12)
