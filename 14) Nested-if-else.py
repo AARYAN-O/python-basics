4c0de6b28be82f55d9f7cc22872c1bc6e988429c
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-06-26 10:26:48 +0530

    Create 14) Nested-if-else.py

---
+++ 14) Nested-if-else.py
@@ -0,0 +1,14 @@
+x=int(input("enter the number"))
+
+if x>0:
+    if x%2==0:
+        print("positive even")
+    else:
+        print("positive odd")
+if x<0:
+    if x%2==0:
+        print("negative even")
+    else:
+        print("negative odd")
+        
+# we just need to have one else statement
