53102bd19dea8fc22c084eb851a6e46e60cb4a80
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-06-26 12:31:07 +0530

    Create 21) default_parameters.py

---
+++ 21) default_parameters.py
@@ -0,0 +1,8 @@
+# just like default arguments,default parameters should also be written at the end.
+
+def pos(city,empno,ename):
+    print(ename)
+    print(empno)
+    print(city)
+    
+pos(ename='Amit',empno=1001,city='hyd')
