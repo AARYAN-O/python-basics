8b3c4021a026f9bb47f73acb9201252bb824132c
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-06-26 10:57:46 +0530

    Create 16) Functions.py

---
+++ 16) Functions.py
@@ -0,0 +1,10 @@
+def func(a,b):
+    return a+b
+
+a=int(input("enter first number"))
+b=int(input("enter second number"))
+
+print(func(a,b))
+
+# functions can have zero or n number of parameters
+# functions can have zero or n number of return statements but only one return statement can be executed at a time.
