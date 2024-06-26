158446f3c343feb9293d879ebda9e365575dacf1
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-06-26 12:33:02 +0530

    Update mix_of_keyword_and_positional_arguments.py

--- mix_of_keyword_and_positional_arguments.py
+++ mix_of_keyword_and_positional_arguments.py
@@ -11,3 +11,7 @@ pos(23,ename='Amit',empno=1001,city='hyd')
 # in the line just above all the keyword arguments must be at the end.
 # Conclusion:in both function call and function definition , we need to have all the positional parameters should 
 # be at the end.
+
+
+# note : When we are having keyword arguments/parameters, we don't need to think a lot about the position of them 
+# but when we are having positional arguments/parameters, we need to think about the position of them.
