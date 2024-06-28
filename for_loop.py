e6ea9cf644ef9461ac2ca073ef911b0f4387ad3d
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-06-28 12:34:18 +0530

    Create for_loop.py

---
+++ for_loop.py
@@ -0,0 +1,17 @@
+for i in range(10):
+    print(i)
+
+# if the starting value in range is not given , then consider it to be 0.
+# The lower limit in the range function is inclusive while the upper limit is not inclusive.
+for i in range(1,10):
+    print(i)
+
+# STEP FUNCTION :
+
+# step function indicates how much gap should be there between values in range during each iteration
+for i in range(1,10,2):
+    print(i)
+
+# what is the default value in step size?
+# the default value in step size is 1
+
