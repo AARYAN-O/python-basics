057a0bc491949f8dca525420a07323a4c5a18654
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-06-28 12:20:15 +0530

    Create 31) __name__==main().py

---
+++ 31) __name__==main().py
@@ -0,0 +1,12 @@
+def Main():
+    print("its the main function")
+    
+if __name__=="__main__":
+    Main()
+
+Main()
+
+# if __name__ == "__main__":
+# print ("Executed when invoked directly")
+# else:
+# print ("Executed when imported")
