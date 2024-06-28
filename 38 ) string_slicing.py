23e585df266f63c5588420cf8b0406ad024bcfca
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-06-28 14:53:34 +0530

    Create 38 ) string_slicing.py

---
+++ 38 ) string_slicing.py
@@ -0,0 +1,26 @@
+# string is immutable
+
+s1="Welcome to De"
+
+s1[0]
+# W
+
+# -1 indicates the last element of the string.
+s1[-1]
+# e
+
+s1[1:]
+elcome to De
+
+s1[:4]
+# Welc
+
+s1[0:8:2]
+#Wloe
+
+# s1[::] -> means that start is 0 and end is the last value
+
+s1[:10]
+# Welcome to
+
+# Smart way of using the len statements 
