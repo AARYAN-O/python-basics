eebf5bdabc62dd559b162982fde9cb42c043634b
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-06-28 13:37:48 +0530

    Create reversing_string.py

---
+++ reversing_string.py
@@ -0,0 +1,13 @@
+Note : We cannot use reversed or reverse here 
+
+# reversing the string
+s1[::-1]
+
+
+s="ahkdbs"
+
+print(reversed(s))
+# reversed object at <0xsdiveok>
+
+s.reverse()
+# AttributeError: 'str' object has no attribute 'reverse'
