bd96ad18d4301254a30d7fbad505e61cd2af817c
Aaryan Ohekar <95522590+AARYAN-O@users.noreply.github.com>
2024-07-05 11:43:59 +0530

    Update Xtra string functions.py

--- Xtra string functions.py
+++ Xtra string functions.py
@@ -1,1 +1,19 @@
-# pelase fill ths 
+# if we write s.split() without collecting the values in some variable.
+# nothing will happen.
+# split() acutally returns a list 
+# wherever the split happens , the comma inside the list will be present there
+
+s3="welcome to to to \n to Del"
+s3.split("\n")
+print(s3)
+# welcome to to to 
+ # to Deloitte
+
+s3="welcome to to to \n to Del"
+y=s3.split("\n")
+print(y)
+# ['welcome to to to' ,'to Deloitte']
+
+n=s3.splitlines()
+print(n)
+# ['welcome to to to ', ' to Deloitte']
